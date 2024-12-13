257607137a1be5e0e7a8cc2fef640bcfb4124c25
eugenikus8 <eugenikus8@gmail.com>
2024-12-14 00:24:03 +0200

    add a current link to the program

--- Filtration.Parser/Services/ItemFilterScriptTranslator.cs
+++ Filtration.Parser/Services/ItemFilterScriptTranslator.cs
@@ -285,7 +285,7 @@ namespace Filtration.Parser.Services
         {
             var outputString = string.Empty;
 
-            outputString += "# Script edited with Filtration - https://github.com/ben-wallis/Filtration" +
+            outputString += "# Script edited with Filtration - https://github.com/eugenikus8/Filtration-POE-Fork" +
                             Environment.NewLine;
 
             if (script.ItemFilterScriptSettings.BlockGroupsEnabled)
